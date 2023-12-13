a962b0ea6b71fc92fadab877b27a9838aec66c83
Qaisar <kayser.abbas@gmail.com>
2023-12-13 14:26:10 +0500

    Update the code

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def random
-    @greeting = Greeting.order(Arel.sql('RANDOM()')).first
-    render json: @greeting.greeting
+    @greeting = Greeting.order('RANDOM()').first
+    render json: @greeting
   end
 end
