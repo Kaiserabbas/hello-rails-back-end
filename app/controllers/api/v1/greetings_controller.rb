50b7968774c2061bfe0eec23f54369e4e5f871c3
Qaisar <kayser.abbas@gmail.com>
2023-12-13 09:27:25 +0500

    Fix linters error

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
-    def random
-        @greeting = Greeting.order(Arel.sql('RANDOM()')).first
-        render json: @greeting::greeting
-    end
+  def random
+    @greeting = Greeting.order(Arel.sql('RANDOM()')).first
+    render json: @greeting.greeting
+  end
 end
