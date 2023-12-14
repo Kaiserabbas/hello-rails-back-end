b9596d02309222379e8e936fd0def1445d2ed9f9
Kaiserabbas <123939543+Kaiserabbas@users.noreply.github.com>
2023-12-14 06:16:01 +0500

    Update the greetings controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::GreetingsController < ApplicationController
   def random
     @greeting = Greeting.order('RANDOM()').first
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
     render json: @greeting
   end
 end
